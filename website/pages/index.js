271bc8419fcaf71430a281f39c40fb0e421b1de5
Alec Manabat <alecmanabat1@gmail.com>
2018-09-08 20:22:29 -0400

    test

--- website/pages/index.js
+++ website/pages/index.js
@@ -6,11 +6,24 @@ const linkStyle = {
 
 const Index = () => (
   <div>
-  <Link href="/lights">
-    <a style={linkStyle}>Light Controls</a>
-  </Link>
-    <p>Hello</p>
-    <img src="/static/bug.jpg" alt=">:[" height="100" width="100"/>
+    <Head>
+      <title>Homepage</title>
+      <meta name="viewport"
+            content="user-scalable=0, initial-scale=1, minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+      />
+    </Head>
+      <React.Fragment>
+        <CssBaseline />
+        {
+          <div>
+            <Link href="/lights">
+                <a style={linkStyle}>Light Controls</a>
+                </Link>
+            <p>Hello</p>
+            <img src="/static/bug.jpg" alt=">:[" height="300" width="300"/>
+          </div>
+        }
+      </React.Fragment>
   </div>
 )
 
