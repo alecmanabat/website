4fecada664bd39bf95af768d55f505cd8543d32b
Alec Manabat <alecmanabat1@gmail.com>
2018-09-30 12:14:27 -0400

    WIP

--- lights/server.js
+++ lights/server.js
@@ -1,19 +1,15 @@
 var SerialPort = require('serialport');
-const fs = require('fs');
-const https = require('https');
-const WebSocket = require('ws');
-
-const server = new https.createServer({
-  cert: fs.readFileSync('cert.pem'),
-  key: fs.readFileSync('key.pem')
-});
-const wss = new WebSocket.Server({ server });
+var request = require('request');
 
 var portName = "COM3";
 
-var connections = new Array;            // list of connections to the server
+var myPort = new SerialPort(portName, 9600, function (err) {
+  if (err) {
+    console.log("error: " + err.message);
+  }
+});
 
-var myPort = new SerialPort(portName, 9600);
+
 var Readline = SerialPort.parsers.Readline; // make instance of Readline parser
 var parser = new Readline(); // make a new parser to read ASCII lines
 myPort.pipe(parser); // pipe the serial stream to the parser
@@ -22,6 +18,12 @@ myPort.on('open', showPortOpen);
 parser.on('data', readSerialData);
 myPort.on('close', showPortClose);
 myPort.on('error', showError);
+
+process.on( 'SIGINT', function() {
+  console.log( "\ngracefully shutting down from  SIGINT (Crtl-C)" )
+  // send off signal to arduino
+  process.exit( )
+})
 
 function showPortOpen(err) {
    console.log('port open. Data rate: ' + myPort.baudRate);
@@ -32,9 +34,7 @@ function showPortOpen(err) {
 
 function readSerialData(data) {
    console.log(data);
-   if (connections.length > 0) {
-    broadcast(data);
-   }
+
 }
 
 function sendToSerial(data) {
@@ -55,25 +55,17 @@ function showError(error) {
    console.log('Serial port error: ' + error);
 }
 
-wss.on('connection', handleConnection);
+function getOption() {
+  /*
 
-function handleConnection(client) {
-  console.log("New Connection");        // you have a new client
-  connections.push(client);             // add this client to the connections array
-
-  client.on('message', sendToSerial);      // when a client sends a message,
-
-  client.on('close', function() {           // when a client closes its connection
-    console.log("connection closed");       // print it out
-    var position = connections.indexOf(client); // get the client's position in the array
-    connections.splice(position, 1);        // and delete it from the array
-  });
-}
-// This function broadcasts messages to all webSocket clients
-function broadcast(data) {
-  for (c in connections) {     // iterate over the array of connections
-    connections[c].send(JSON.stringify(data)); // send the data to each connection
-  }
+request.get('http://www.whatever.com/my.csv', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var csv = body;
+        // Continue with your processing here.
+    }
+});
+  //put the option in a file or put it on a separate page
+  */
 }
 
-server.listen(8081);
+setInterval(getOption, 100);
